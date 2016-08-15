16ff7b52427cb01572f63d5a3a5ad6737c0286e3
Tom Martin <tom@tpgmartin.com>
2016-08-15 23:09:04 +0100

    Publish/subscribe to state changes

--- src/server.js
+++ src/server.js
@@ -2,4 +2,12 @@ import Server from 'socket.io'
 
 export default function startServer () {
   const io = new Server().attach(8090)
+
+  store.subscribe(
+    () => io.emit('state', store.getState().toJS())
+  )
+
+  io.on('connection', (socket) => {
+    socket.emit('state', store.getState().toJS())
+  })
 }
